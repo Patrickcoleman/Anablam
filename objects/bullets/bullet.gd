5f43dea5f3b3296ec795ffc6c676a15769bb8d47
Patrick Coleman <sirpatrickcoleman@gmail.com>
2026-08-13 15:10:41 +0200

    Reverted bullet debugging changes

--- objects/bullets/bullet.gd
+++ objects/bullets/bullet.gd
@@ -2,9 +2,9 @@ extends Area2D
 class_name Bullet
 
 var speed: float = 600.0
-#var owner_peer_id: int = -1
+var owner_peer_id: int = -1
 
-const SPRITES: Array[CompressedTexture2D] = [
+const SPRITES: Array[Texture2D] = [
 	preload("res://objects/bullets/bulletGreenSilver_outline.png"),
 	preload("res://objects/bullets/bulletRedSilver_outline.png"),
 	preload("res://objects/bullets/bulletSilverSilver_outline.png"),
@@ -17,7 +17,7 @@ func _physics_process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if !multiplayer.is_server(): return
-	#if body is Player && body.peer_id == owner_peer_id:
-		#return
+	if body is Player && body.peer_id == owner_peer_id:
+		return
 	
 	queue_free()
